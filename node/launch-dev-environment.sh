bd593ca55c754b7b924a9f8325c6ef7243eb33bc
Hugo Pinto <devdepijama@gmail.com>
2022-03-03 20:18:33 -0300

    Add a script for using node/npm without instaling

---
+++ node/launch-dev-environment.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Launches a container with node15
+# mounting ./src folder in /src (container path)
+docker run -it \
+           --name node-dev \
+           --workdir /src \
+           --mount type=bind,source="$(pwd)"/src,target=/src \
+           --rm \
+           -p "3000:3000" \
+           node:15 \
+           bash
