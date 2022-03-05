0b987318b50d6c7552601d9c8209c5cfe56fd92d
Hugo Pinto <devdepijama@gmail.com>
2022-03-05 17:50:29 -0300

    Insert dynamic data into templated view

--- node/src/index.js
+++ node/src/index.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const people = [
+    "Jeff Bezos",
+    "Elon Musk",
+    "Bill Gates",
+    "Paulo Lemann"
+]
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('pages/index',
+    {
+        "title": "Full Cycle Rocks!",
+        "people": people
+    })
 })
 
 app.listen(port, () => {
