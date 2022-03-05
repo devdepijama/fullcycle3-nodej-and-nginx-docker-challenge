daf5417f3222379014d123d5e6a7a89ef1dc139a
Hugo Pinto <devdepijama@gmail.com>
2022-03-05 18:22:50 -0300

    Insert data into table when initiating database

--- mysql/01_create_empty_table.sql
+++ mysql/01_create_empty_table.sql
@@ -2,3 +2,8 @@ CREATE TABLE People(
     PersonID int,
     Name varchar(255)
 );
+
+INSERT INTO People (PersonID, Name) VALUES(0, 'Jeff Bezzos');
+INSERT INTO People (PersonID, Name) VALUES(1, 'Bill Gates');
+INSERT INTO People (PersonID, Name) VALUES(2, 'Elon Musk');
+INSERT INTO People (PersonID, Name) VALUES(3, 'Paulo Lemann');
